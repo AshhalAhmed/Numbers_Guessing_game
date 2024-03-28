467a7503094c017b762a4a18b002a809df1c5675
ASHHAL AHMED <Ashhalahmed8@gmail.com>
2024-03-28 19:49:41 +0500

    first commit

---
+++ numberguess.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber=Math.floor(Math.random()*6);
+const numberGuess=await inquirer.prompt([
+    {
+        message:"Enter a Number between 0 - 6",
+        name: 'numberGuessing',
+        type: 'number'
+    }
+])
+if(numberGuess.numberGuessing===randomNumber){
+    console.log("Congtratulations!! You guessed Right number ")
+}else {
+    console.log(`You guessed the wrong number. The correct number was ${randomNumber} `);
+}
